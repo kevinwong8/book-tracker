8a0b6d01fbcd11fdeae71fe333cde84c0cf72a57
kevinwong <kevingreg88@gmail.com>
2024-06-23 10:45:26 +0900

    Lab 1 finished

--- book_tracker/settings.py
+++ book_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h-^#r0t+s#d14(wnrecs813ihk!#-bzux*@_c2-tgot1syh-&4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main'
 ]
 
 MIDDLEWARE = [
