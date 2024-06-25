1ae26defbc833aa746a117cfa21cde9ea9715cd0
kevinwong <kevingreg88@gmail.com>
2024-06-25 17:29:23 +0900

    login, logout, cookies and connecting the book and user

--- main/views.py
+++ main/views.py
@@ -3,16 +3,25 @@ from main.forms import BookForm
 from main.models import Book
 from django.http import HttpResponse
 from django.core import serializers
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+import datetime
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 
 # Create your views here.
+@login_required(login_url='/login')
 def show_main(request):
-    books = Book.objects.all()
+    books = Book.objects.filter(user= request.user)
     
     context = {
-        'name' : 'Pak Bepe',
+        'name' : request.user.username,
         'class' : "PBP A",
-        'books' : books
+        'books' : books,
+        'last_login': request.COOKIES['last_login']
     }
     return render(request, "main.html", context)
 
@@ -20,11 +29,49 @@ def create_book(request):
     form = BookForm(request.POST or None)
 
     if form.is_valid() and request.method == "POST":
-        form.save()
+        book = form.save(commit=False)
+        book.user = request.user
+        book.save()
         return redirect('main:show_main')
     
     context = {'form' : form}
     return render(request, 'create_book.html', context)
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+        
+    context = {'form': form}
+    return render(request, 'register.html', context)
+    
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:show_main"))
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+        else:
+            messages.info(request, 'Sorry incorrect username or password. Please try again.')
+
+    context= {}
+    return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse("main:login"))
+    response.delete_cookie("last_login")
+    return response
+
 
 def show_xml(request):
     data = Book.objects.all()
